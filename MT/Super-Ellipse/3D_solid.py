524f25ea64d6c4b03b331e6d2753762a90fd3fa0
Akshay <nair_akshay@rocketmail.com>
2025-10-15 00:11:27 +0200

    Update

--- MT/Super-Ellipse/3D_solid.py
+++ MT/Super-Ellipse/3D_solid.py
@@ -26,9 +26,9 @@ model.rootAssembly.regenerate()
 a, b, c = 150.0, 150.0, 150.0   # inner semi-axes
 total_length = c
 t = 2.5                         # total thickness
-n1, n2 = 2, 2                   # shape exponents
+n1, n2 = 12, 12                   # shape exponents
 num_points = 30                 # points per curve
-num_layers = 4                  # number of layers through thickness
+num_layers = 1                  # number of layers through thickness
 num_partitions = 4               # number of partitions
 
 a_out, b_out, c_out = a + t, b + t, c + t  # outer semi-axes
@@ -68,8 +68,9 @@ def create_layer_part(model, layer_index):
     phi_vals = [math.radians(i*90/num_points) for i in range(num_points+1)]
     theta_vals = [math.radians(i*90/num_points) for i in range(num_points+1)]
     # --- Case 1 (phi=0) ---
-    inner_case1 = [superellipsoid_point_3d(0.0, th, a_i, b_i, c_i, n1, n2) for th in theta_vals]
-    outer_case1 = [superellipsoid_point_3d(0.0, th, a_o, b_o, c_o, n1, n2) for th in theta_vals]
+    theta_sp = [math.radians(90) - math.radians(i*45/num_points) for i in range(num_points+1)]
+    inner_case1 = [superellipsoid_point_3d(0.0, th, a_i, b_i, c_i, n1, n2) for th in theta_sp]
+    outer_case1 = [superellipsoid_point_3d(0.0, th, a_o, b_o, c_o, n1, n2) for th in theta_sp]
     # --- Case 2 (theta=0) ---
     inner_case2 = [superellipsoid_point_3d(ph, 0.0, a_i, b_i, c_i, n1, n2) for ph in phi_vals]
     outer_case2 = [superellipsoid_point_3d(ph, 0.0, a_o, b_o, c_o, n1, n2) for ph in phi_vals]
@@ -77,27 +78,38 @@ def create_layer_part(model, layer_index):
     theta_90 = math.radians(90)
     inner_case3 = [superellipsoid_point_3d(ph, theta_90, a_i, b_i, c_i, n1, n2) for ph in phi_vals]
     outer_case3 = [superellipsoid_point_3d(ph, theta_90, a_o, b_o, c_o, n1, n2) for ph in phi_vals]
+    # --- Case 4 (theta=45°) ---
+    theta_45 = math.radians(45)
+    inner_case4 = [superellipsoid_point_3d(ph, theta_45, a_i, b_i, c_i, n1, n2) for ph in phi_vals]
+    outer_case4 = [superellipsoid_point_3d(ph, theta_45, a_o, b_o, c_o, n1, n2) for ph in phi_vals]
     # Fix overlapping endpoints
-    inner_case2[-1] = inner_case3[-1]
-    outer_case2[-1] = outer_case3[-1]
+    """inner_case2[-1] = inner_case3[-1]
+    outer_case2[-1] = outer_case3[-1]"""
+    inner_case3[-1] = inner_case2[-1]
+    outer_case3[-1] = outer_case2[-1]
+    inner_case4[-1] = inner_case2[-1]
+    outer_case4[-1] = outer_case2[-1]
     part_name = "Layer_" + str(layer_index + 1)
     p = model.Part(name=part_name, dimensionality=THREE_D, type=DEFORMABLE_BODY)
     # Create datum points for each case
     create_datums(p, inner_case1 + outer_case1)
-    create_datums(p, inner_case2 + outer_case2)
+    """create_datums(p, inner_case2 + outer_case2)"""
     create_datums(p, inner_case3 + outer_case3)
+    create_datums(p, inner_case4 + outer_case4)
     # Create wire splines for each case
     create_wire_splines(p, inner_case1)
     create_wire_splines(p, outer_case1)
-    create_wire_splines(p, inner_case2)
-    create_wire_splines(p, outer_case2)
+    """create_wire_splines(p, inner_case2)
+    create_wire_splines(p, outer_case2)"""
     create_wire_splines(p, inner_case3)
     create_wire_splines(p, outer_case3)
+    create_wire_splines(p, inner_case4)
+    create_wire_splines(p, outer_case4)
     mid_idx = len(inner_case1) // 2
     e1 = p.edges
-    p.SolidLoft(loftsections=((e1.findAt(coordinates=(inner_case1[mid_idx])), e1.findAt(coordinates=(inner_case2[mid_idx])), e1.findAt(coordinates=( inner_case3[mid_idx]))), 
+    """p.SolidLoft(loftsections=((e1.findAt(coordinates=(inner_case1[mid_idx])), e1.findAt(coordinates=(inner_case2[mid_idx])), e1.findAt(coordinates=( inner_case3[mid_idx]))), 
                           (e1.findAt(coordinates=(outer_case1[mid_idx])), e1.findAt(coordinates=(outer_case2[mid_idx])), e1.findAt( coordinates=(outer_case3[mid_idx])))), 
-                          startCondition=NONE, endCondition=NONE)
+                          startCondition=NONE, endCondition=NONE)"""
     return p
 
 model = mdb.models['SuperEllipse']
@@ -364,28 +376,24 @@ model.StaticStep(name='LoadingStep', previous='Initial',
 
 ############ Create Surface ############
 
-theta_vals = []
+phi_surf = []
 
-th_o = math.radians(90)
-x = (phi_vals[0] - 0.0) / 2.0
-theta_vals.append(th_o - (phi_vals[0] - x))
+for i in range(0, len(phi_vals)):
+    phi_surf.append(phi_vals[i] - math.radians(0.5))
 
-for i in range(1, len(phi_vals)):
-    x = (phi_vals[i] - phi_vals[i-1]) / 2.0
-    theta_vals.append(th_o - (phi_vals[i] - x))
+ph_o = math.radians(90)
+x = (ph_o - phi_vals[-1])/2
+phi_surf.append(ph_o - x)
 
-x = (th_o - phi_vals[-1]) / 2.0
-theta_vals.append(th_o - (th_o - x))
-
-# --- Assembly and faces
 a1 = mdb.models['SuperEllipse'].rootAssembly
 s1 = a1.instances['SuperEllipsoid-1'].faces
 search_tol = 0.1
 faces_combined = []
 
-for i, theta_i in enumerate(theta_vals):
-    phi_i = phi_vals[0]
+for i, phi_i in enumerate(phi_surf):
+    theta_i = math.radians(45)
     pt = superellipsoid_point_3d(phi_i, theta_i, a, b, c, n1, n2)
+    p.DatumPointByCoordinate(coords=pt)
     found = s1.getClosest(coordinates=(tuple(pt),), searchTolerance=search_tol)
     if found:
         face_pt = found[0][1]
@@ -394,7 +402,6 @@ for i, theta_i in enumerate(theta_vals):
             # a1.Surface(side1Faces=seq_face, name='Surf_%d' % (i+1))
             faces_combined.append(seq_face)
 
-# Create a single combined surface
 if faces_combined:
     a1.Surface(side1Faces=faces_combined, name='Surf_Load')
 
@@ -409,7 +416,7 @@ theta_i = math.radians(45)
 pt = superellipsoid_point_3d(phi_i, theta_i, a, b, c, n1, n2)
 a1.DatumPointByCoordinate(coords=pt)
 
-phi_i = math.radians(85)
+phi_i = 1.1693705988362
 theta_i = math.radians(45)
 pt = superellipsoid_point_3d(phi_i, theta_i, a, b, c, n1, n2)
-a1.DatumPointByCoordinate(coords=pt)"""
+p.DatumPointByCoordinate(coords=pt)"""
